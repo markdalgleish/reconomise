23b3fd4b332b04365e692ab9e549591ea63dcc08
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2012-06-02 16:17:26 +1000

    Hook up public dir, fix config module name

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
 var express = require('express'),
 	mongoose = require('mongoose'),
 	_ = require('underscore')._,
-	config = require('./config.js');
+	config = require('./server-config.js');
 
 var app = express.createServer();
+
+app.configure(function(){
+	app.use(express.methodOverride());
+	app.use(express.bodyParser());
+	app.use(app.router);
+	app.use(express.static(__dirname + '/public'));
+	app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
 
 app.get('/', function(req, res){
     res.send('Hello World');
 });
 
 var PORT = 3000;
-console.log(config.sensis_apikey);
 console.log('Listening on port ' + PORT + '...');
 app.listen(PORT);
